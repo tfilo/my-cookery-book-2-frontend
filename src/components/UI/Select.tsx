7b61b09dcf7cf06aa494c586717c78af62d6fcfb
Tomáš Filo <filo@microcomp.sk>
2022-12-06 10:48:49 +0100

    Fix selectu

--- src/components/UI/Select.tsx
+++ src/components/UI/Select.tsx
@@ -25,73 +25,114 @@ type SelectProps = {
 const Select: React.FC<SelectProps> = (props) => {
     const id = useId();
     const {
+        register,
         formState: { errors },
-        control,
     } = useFormContext();
 
-    //const errorMessage = errors[props.name]?.message;
     const errorMessage = get(errors, props.name)?.message;
 
     return (
-        <Controller
-            name={props.name}
-            control={control}
-            render={({ field }) => (
-                <Form.Group className='mb-3' controlId={`${id}_${props.name}`}>
-                    <Form.Label>{props.label}</Form.Label>
-                    <Form.Select
-                        {...field}
-                        value={
-                            Array.isArray(field.value)
-                                ? field.value.map((v) => v.toString())
-                                : field.value?.toString() ?? '-1'
-                        }
-                        disabled={props.disabled}
-                        multiple={props.multiple}
-                        isInvalid={!!errorMessage}
-                    >
-                        {!props.multiple && (
-                            <option value='-1' disabled>
-                                Prosím zvolte možnosť
-                            </option>
-                        )}
-
-                        {props.options?.map((option) => {
-                            if ('options' in option) {
-                                return (
-                                    <optgroup
-                                        key={option.optGroupId}
-                                        label={option.optGroupName}
-                                    >
-                                        {option.options.map((opt) => (
-                                            <option
-                                                key={opt.value}
-                                                value={opt.value}
-                                            >
-                                                {opt.label}
-                                            </option>
-                                        ))}
-                                    </optgroup>
-                                );
-                            } else {
-                                return (
-                                    <option
-                                        key={option.value}
-                                        value={option.value}
-                                    >
-                                        {option.label}
+        <Form.Group className='mb-3' controlId={`${id}_${props.name}`}>
+            <Form.Label>{props.label}</Form.Label>
+            <Form.Select
+                {...register(props.name)}
+                disabled={props.disabled}
+                multiple={props.multiple}
+                isInvalid={!!errorMessage}
+            >
+                {!props.multiple && (
+                    <option value='-1' disabled>
+                        Prosím zvolte možnosť
+                    </option>
+                )}
+                {props.options.map((option) => {
+                    if ('options' in option) {
+                        return (
+                            <optgroup
+                                key={option.optGroupId}
+                                label={option.optGroupName}
+                            >
+                                {option.options.map((opt) => (
+                                    <option key={opt.value} value={opt.value}>
+                                        {opt.label}
                                     </option>
-                                );
-                            }
-                        })}
-                    </Form.Select>
-                    <Form.Control.Feedback type='invalid'>
-                        {errorMessage?.toString()}
-                    </Form.Control.Feedback>
-                </Form.Group>
-            )}
-        />
+                                ))}
+                            </optgroup>
+                        );
+                    } else {
+                        return (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        );
+                    }
+                })}
+            </Form.Select>
+            <Form.Control.Feedback type='invalid'>
+                {errorMessage?.toString()}
+            </Form.Control.Feedback>
+        </Form.Group>
     );
+
+    // return (
+    //     <Controller
+    //         name={props.name}
+    //         control={control}
+    //         render={({ field }) => (
+    //             <Form.Group className='mb-3' controlId={`${id}_${props.name}`}>
+    //                 <Form.Label>{props.label}</Form.Label>
+    //                 <Form.Select
+    //                     onChange={(e) => field.onChange(e)}
+    //                     onBlur={() => field.onBlur()}
+    //                     name={field.name}
+    //                     ref={field.ref}
+    //                     value={logAndReturn(field.value)}
+    //                     disabled={props.disabled}
+    //                     multiple={props.multiple}
+    //                     isInvalid={!!errorMessage}
+    //                 >
+    //                     {!props.multiple && (
+    //                         <option value='-1' disabled>
+    //                             Prosím zvolte možnosť
+    //                         </option>
+    //                     )}
+
+    //                     {props.options?.map((option) => {
+    //                         if ('options' in option) {
+    //                             return (
+    //                                 <optgroup
+    //                                     key={option.optGroupId}
+    //                                     label={option.optGroupName}
+    //                                 >
+    //                                     {option.options.map((opt) => (
+    //                                         <option
+    //                                             key={opt.value}
+    //                                             value={opt.value}
+    //                                         >
+    //                                             {opt.label}
+    //                                         </option>
+    //                                     ))}
+    //                                 </optgroup>
+    //                             );
+    //                         } else {
+    //                             return (
+    //                                 <option
+    //                                     key={option.value}
+    //                                     value={option.value}
+    //                                 >
+    //                                     {option.label}
+    //                                 </option>
+    //                             );
+    //                         }
+    //                     })}
+    //                 </Form.Select>
+    //                 <Form.Control.Feedback type='invalid'>
+    //                     {errorMessage?.toString()}
+    //                 </Form.Control.Feedback>
+    //             </Form.Group>
+    //         )}
+    //     />
+    // );
 };
 
 export default Select;
