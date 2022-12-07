c514d5e5c57c730fe46349c0545d4d4fa6b0f480
Tomáš Filo <filo@microcomp.sk>
2022-12-07 14:00:16 +0100

    Fixed select

--- src/components/UI/Select.tsx
+++ src/components/UI/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useId } from 'react';
+import React, { useEffect, useId } from 'react';
 import { Form } from 'react-bootstrap';
 import { useFormContext } from 'react-hook-form';
 import { get } from 'lodash';
@@ -27,9 +27,28 @@ const Select: React.FC<SelectProps> = (props) => {
     const {
         register,
         formState: { errors },
+        getValues,
+        setValue,
     } = useFormContext();
 
     const errorMessage = get(errors, props.name)?.message;
+
+    const value = getValues(props.name);
+
+    useEffect(() => {
+        if (props.multiple) {
+            if (Array.isArray(value)) {
+                setValue(
+                    props.name,
+                    value.filter((v) => !!v).map((v) => v.toString())
+                );
+            } else if (!!value) {
+                setValue(props.name, value ? [value.toString()] : []);
+            }
+        } else {
+            setValue(props.name, value ? value.toString() : '-1');
+        }
+    }, [value, props.multiple]);
 
     return (
         <Form.Group className='mb-3' controlId={`${id}_${props.name}`}>
@@ -73,66 +92,6 @@ const Select: React.FC<SelectProps> = (props) => {
             </Form.Control.Feedback>
         </Form.Group>
     );
-
-    // return (
-    //     <Controller
-    //         name={props.name}
-    //         control={control}
-    //         render={({ field }) => (
-    //             <Form.Group className='mb-3' controlId={`${id}_${props.name}`}>
-    //                 <Form.Label>{props.label}</Form.Label>
-    //                 <Form.Select
-    //                     onChange={(e) => field.onChange(e)}
-    //                     onBlur={() => field.onBlur()}
-    //                     name={field.name}
-    //                     ref={field.ref}
-    //                     value={logAndReturn(field.value)}
-    //                     disabled={props.disabled}
-    //                     multiple={props.multiple}
-    //                     isInvalid={!!errorMessage}
-    //                 >
-    //                     {!props.multiple && (
-    //                         <option value='-1' disabled>
-    //                             Prosím zvolte možnosť
-    //                         </option>
-    //                     )}
-
-    //                     {props.options?.map((option) => {
-    //                         if ('options' in option) {
-    //                             return (
-    //                                 <optgroup
-    //                                     key={option.optGroupId}
-    //                                     label={option.optGroupName}
-    //                                 >
-    //                                     {option.options.map((opt) => (
-    //                                         <option
-    //                                             key={opt.value}
-    //                                             value={opt.value}
-    //                                         >
-    //                                             {opt.label}
-    //                                         </option>
-    //                                     ))}
-    //                                 </optgroup>
-    //                             );
-    //                         } else {
-    //                             return (
-    //                                 <option
-    //                                     key={option.value}
-    //                                     value={option.value}
-    //                                 >
-    //                                     {option.label}
-    //                                 </option>
-    //                             );
-    //                         }
-    //                     })}
-    //                 </Form.Select>
-    //                 <Form.Control.Feedback type='invalid'>
-    //                     {errorMessage?.toString()}
-    //                 </Form.Control.Feedback>
-    //             </Form.Group>
-    //         )}
-    //     />
-    // );
 };
 
 export default Select;
