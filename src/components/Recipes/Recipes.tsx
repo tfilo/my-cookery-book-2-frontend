8dfaf23d14141cb607122f81bd590484ed8a5af4
petrafilova <petra.petrasova555@gmail.com>
2022-12-19 10:45:04 +0100

    pagination

--- src/components/Recipes/Recipes.tsx
+++ src/components/Recipes/Recipes.tsx
@@ -1,16 +1,31 @@
-import React, { Fragment, useEffect, useMemo, useState } from 'react';
-import { Button, Card, Col, /*Form,*/ Pagination, Row } from 'react-bootstrap';
+import React, {
+    Fragment,
+    useCallback,
+    useEffect,
+    useMemo,
+    useState,
+} from 'react';
+import { Button, Card, Col, Pagination, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { Api } from '../../openapi';
 import { pictureApi, recipeApi } from '../../utils/apiWrapper';
 import Modal from '../UI/Modal';
 import defImg from '../../assets/defaultRecipe.jpg';
 import { formatErrorMessage } from '../../utils/errorMessages';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFilter } from '@fortawesome/free-solid-svg-icons';
-// import Input from '../UI/Input';
-// import { FormProvider, useForm, SubmitHandler } from 'react-hook-form';
-// import Spinner from '../UI/Spinner';
+
+
+type RecipeWithUrl = {
+    page: number;
+    pageSize: number;
+    rows: {
+        id: number;
+        name: string;
+        description: string | null;
+        url?: string;
+        pictures: Api.SimpleRecipe.Picture[];
+    }[];
+    count: number;
+};
 
 const Recipes: React.FC = () => {
     const [error, setError] = useState<string>();
@@ -18,21 +33,7 @@ const Recipes: React.FC = () => {
     const [list, setList] = useState<RecipeWithUrl>();
     const [currentPage, setCurrentPage] = useState(1);
     const [numberOfPages, setNumberOfPages] = useState<number>();
-    // const [showFilter, setShowFilter] = useState<boolean>(false);
     const [arrayOfPageNumbers, setArrayOfPageNumbers] = useState<number[]>();
-
-    type RecipeWithUrl = {
-        page: number;
-        pageSize: number;
-        rows: {
-            id: number;
-            name: string;
-            description: string | null;
-            url?: string;
-            pictures: Api.SimpleRecipe.Picture[];
-        }[];
-        count: number;
-    };
 
     const data: Api.RecipeSearchCriteria = useMemo(() => {
         return {
@@ -46,104 +47,35 @@ const Recipes: React.FC = () => {
         };
     }, [currentPage]);
 
-    // const methods = useForm();
+    const pagesToShow = 5;
+    const calculateArray = useCallback(
+        (currentPage: number, numberOfPages: number) => {
+            const showNumbers: number[] = [];
+            if (numberOfPages) {
+                const firstPage = Math.max(
+                    Math.min(
+                        currentPage - Math.floor((pagesToShow - 1) / 2),
+                        numberOfPages - pagesToShow + 1
+                    ),
+                    1
+                );
+                const lastPage = Math.min(
+                    firstPage + (pagesToShow - 1),
+                    numberOfPages
+                );
 
-    // const {
-    //     formState: { isSubmitting },
-    // } = methods;
+                console.log(firstPage, lastPage, currentPage, numberOfPages);
 
-    const pagesToShow = 10;
-
-    const calculateArray = () => {
-        const showNumbers: number[] = [];
-        // for (let i = 1; i <= pagesToShow; i ++) {
-        //     showNumbers.push(i+100)
-        // }
-        if (numberOfPages) {
-                const begginning = Math.max(currentPage - Math.floor((pagesToShow-1)/2), 1);
-                //showNumbers.splice(0, 1, begginning);
-                const last = Math.min(begginning + pagesToShow, numberOfPages);
-                //showNumbers.splice(showNumbers.length-1, 1, last);
-                for (let i = begginning; i <= last; i++) {
+                for (let i = firstPage; i <= lastPage; i++) {
                     showNumbers.push(i);
                 }
-        }
-        console.log(showNumbers);
+            }
+            setArrayOfPageNumbers(showNumbers);
+            console.log(showNumbers);
+        },
+        []
+    );
 
-        // if(list?.pageSize) {
-        //     const trimStart = (currentPage-1) * list?.pageSize;
-        //     const trimEnd = trimStart + list.pageSize;
-        //     console.log(showNumbers.slice(trimStart, trimEnd))
-        // }
-
-        // const pageNumbers = [];
-        // if (numberOfPages) {
-        //     const begginning = currentPage - Math.floor(pagesToShow/2);
-        //     const beg= currentPage - Math.floor(pagesToShow/2) + 1;
-        //     const cur = currentPage;
-        //     const next = currentPage + Math.floor(pagesToShow/2) -1;
-        //     const last = currentPage + Math.floor(pagesToShow/2);
-        //     showNumbers.push(begginning, beg, cur, next, last);
-        // }
-        setArrayOfPageNumbers(showNumbers);
-        console.log(showNumbers);
-
-            
-        // if (numberOfPages) {
-        //     for (let i = 1; i <= numberOfPages; i++) {
-        //         pageNumbers.push(i);
-        //     }
-
-        //     if (pageNumbers.length < 6) {
-        //         showNumbers = [...pageNumbers];
-        //         console.log(showNumbers);
-        //     } else if (
-        //         pageNumbers.length >= 6 &&
-        //         pageNumbers.length < 8 &&
-        //         currentPage === 1
-        //     ) {
-        //         showNumbers.push(
-        //             currentPage,
-        //             currentPage + 1,
-        //             currentPage + 2,
-        //             currentPage + 3,
-        //             currentPage + 4
-        //         );
-        //     } else if (
-        //         pageNumbers.length >= 6 &&
-        //         pageNumbers.length < 8 &&
-        //         currentPage === 2
-        //     ) {
-        //         showNumbers.push(
-        //             currentPage - 1,
-        //             currentPage,
-        //             currentPage + 1,
-        //             currentPage + 2,
-        //             currentPage + 3
-        //         );
-        //     } else if (
-        //         pageNumbers.length >= 6 &&
-        //         pageNumbers.length < 8 &&
-        //         currentPage === 3
-        //     ) {
-        //         showNumbers.push(
-        //             currentPage - 2,
-        //             currentPage - 1,
-        //             currentPage,
-        //             currentPage + 1,
-        //             currentPage + 2
-        //         );
-        //     } else {
-        //         showNumbers.push(
-        //             currentPage - 2,
-        //             currentPage - 1,
-        //             currentPage,
-        //             currentPage + 1,
-        //             currentPage + 2
-        //         );
-        //     }
-        // }
-    };
     useEffect(() => {
         (async () => {
             try {
@@ -178,17 +110,17 @@ const Recipes: React.FC = () => {
                 setNumberOfPages(round);
                 setList(formattedRecipe);
 
-                calculateArray();
-                console.log(
-                    `currentPage: ${currentPage}, page: ${formattedRecipe?.page} count: ${formattedRecipe?.count} pageSize: ${formattedRecipe?.pageSize} numberOfPages: ${numberOfPages}`
-                );
+                calculateArray(recipes.page + 1, round);
+                // console.warn(
+                //     `currentPage: ${currentPage}, page: ${formattedRecipe?.page} count: ${formattedRecipe?.count} pageSize: ${formattedRecipe?.pageSize} numberOfPages: ${numberOfPages}`
+                // );
             } catch (err) {
                 formatErrorMessage(err).then((message) => {
                     setError(message);
                 });
             }
         })();
-    }, [data]);
+    }, [data, calculateArray]);
 
     const createRecipeHandler = () => {
         navigate('/recipe/create');
@@ -224,42 +156,13 @@ const Recipes: React.FC = () => {
         setCurrentPage(pageNumber);
     };
 
-    // const previousPageHandler1 = () => {
-    //     setCurrentPage(currentPage - 1);
-    // };
-    // const previousPageHandler2 = () => {
-    //     setCurrentPage(currentPage - 2);
-    // };
-    // const nextPageHandler1 = () => {
-    //     setCurrentPage(currentPage + 1);
-    // };
-    // const nextPageHandler2 = () => {
-    //     setCurrentPage(currentPage + 2);
-    // };
-
-    // const showFilterHandler = () => {
-    //     setShowFilter(!showFilter);
-    // };
-
-    // const submitHandler: SubmitHandler<data> = async (
-    //     values: data
-    // ) => {
-    //     const sendData = {};
-    // };
+    
+    console.log(list);
 
     return (
         <Fragment>
             <div className='d-flex flex-column flex-md-row'>
                 <h2 className='flex-grow-1'>Recepty</h2>
-                {/* <Button
-                    variant='light'
-                    aria-label='presunúť sekciu nadol'
-                    type='button'
-                    onClick={showFilterHandler}
-                    className='mb-3'
-                >
-                    <FontAwesomeIcon className='text-dark' icon={faFilter} />
-                </Button> */}
                 <Button
                     variant='primary'
                     onClick={createRecipeHandler}
@@ -268,26 +171,6 @@ const Recipes: React.FC = () => {
                     Pridať recept
                 </Button>
             </div>
-            {/* {showFilter && (
-                <FormProvider {...methods}>
-                    <Form
-                        onSubmit={methods.handleSubmit(submitHandler)}
-                        noValidate
-                    >
-                        <Input name='search' label='Názov'></Input>
-                        <Input name='categoryId' label='Kategórie'></Input>
-                        <Input name='tags' label='Značky'></Input>
-                        <Button
-                            variant='primary'
-                            type='submit'
-                            className='mb-3'
-                        >
-                            Pridať recept
-                        </Button>
-                    </Form>
-                </FormProvider>
-            )} */}
-            {/* {isSubmitting && <Spinner />} */}
             <Row xs={1} sm={2} lg={4} className='g-4'>
                 {list?.rows.map((row) => {
                     return (
@@ -343,8 +226,8 @@ const Recipes: React.FC = () => {
                     );
                 })}
             </Row>
-            
-            {list && (list.count / list.pageSize) > 1 && (
+
+            {list && list.count / list.pageSize > 1 && (
                 <Pagination className='justify-content-center'>
                     <Pagination.First
                         onClick={firstPageHandler}
@@ -364,36 +247,6 @@ const Recipes: React.FC = () => {
                             {v}
                         </Pagination.Item>
                     ))}
-
-                    {/* {currentPage !== 1 &&
-                        currentPage === numberOfPages &&
-                        numberOfPages !== 2 && (
-                            <Pagination.Item onClick={previousPageHandler2}>
-                                {currentPage - 2}
-                            </Pagination.Item>
-                        )}
-
-                    {currentPage !== 1 && (
-                        <Pagination.Item onClick={previousPageHandler1}>
-                            {currentPage - 1}
-                        </Pagination.Item>
-                    )}
-
-                    <Pagination.Item active>{currentPage}</Pagination.Item>
-
-                    {currentPage !== numberOfPages && (
-                        <Pagination.Item onClick={nextPageHandler1}>
-                            {currentPage + 1}
-                        </Pagination.Item>
-                    )}
-
-                    {currentPage !== numberOfPages &&
-                        currentPage === 1 &&
-                        numberOfPages !== 2 && (
-                            <Pagination.Item onClick={nextPageHandler2}>
-                                {currentPage + 2}
-                            </Pagination.Item>
-                        )} */}
 
                     <Pagination.Next
                         onClick={nextPageHandler}
